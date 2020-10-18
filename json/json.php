5f15c0933f75ce154e0b539d51e439e275e7f2f4
Алина Пимошенко <pimoshenko63@gmail.com>
2020-10-18 12:25:11 +0300

    last version

--- json/json.php
+++
@@ -1,7 +0,0 @@
-<?php
-// на какие данные рассчитан этот скрипт
-header("Content-Type: application/json");
-// разбираем JSON-строку на составляющие встроенной командой
-$data = json_decode(file_get_contents("php://input"));
-// отправляем в ответ строку с подтверждением
-echo "Сервер получил следующие данные: login — $data->login, password — $data->password, email — $data->email";
